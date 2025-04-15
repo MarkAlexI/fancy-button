421d7c2d8000048d4c5f788c5f7b06a4509bc1d3
MarkAlexI <markovaleksandr108@gmail.com>
2025-04-15 11:33:53 +0300

    Create label property and custom event.

--- dist/fancy-button.js
+++ dist/fancy-button.js
@@ -1,13 +1,30 @@
 class FancyButton extends HTMLElement {
   constructor() {
     super();
-    
-    const shadow = this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: 'open' });
     
     const template = document.getElementById('fancy-button-template');
     const content = template.content.cloneNode(true);
     
-    shadow.appendChild(content);
+    this.shadowRoot.appendChild(content);
+  }
+  
+  connectedCallback() {
+    this.button = this.shadowRoot.querySelector('button');
+    
+    if (this.hasAttribute('label')) {
+      this.button.textContent = this.getAttribute('label');
+    }
+    
+    this.button.addEventListener('click', () => {
+      this.dispatchEvent(new CustomEvent('fancy-click', {
+        bubbles: true,
+        composed: true,
+        detail: {
+          message: 'Fancy Button clicked!'
+        }
+      }));
+    });
   }
 }
 
